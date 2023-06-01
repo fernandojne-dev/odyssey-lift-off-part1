331b34ece7bca37bfd1153713e56c03dd05503a0
Fernando Nuñez <31429242+fernandojne20@users.noreply.github.com>
2023-05-31 23:19:45 -0500

    Chapter-8: Apollo client setup (#4)

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 /** importing our pages */
-import Tracks from './tracks';
+import Tracks from './tracks'
 
-export default function Pages() {
+export default function Pages () {
   return (
     <BrowserRouter>
       <Routes>
-        <Route element={<Tracks />} path="/" />
+        <Route element={<Tracks />} path='/' />
       </Routes>
     </BrowserRouter>
-  );
+  )
 }
